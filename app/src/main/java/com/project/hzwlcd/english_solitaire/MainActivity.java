5c1af5fd997c7e9e4c5d6ff524bea21bf8c33c94
hezw <hzw12345>
2017-10-19 17:44:15 +0800

    新增访问服务器功能

--- app/src/main/java/com/project/hzwlcd/english_solitaire/MainActivity.java
+++ app/src/main/java/com/project/hzwlcd/english_solitaire/MainActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,12 +19,14 @@ public class MainActivity extends AppCompatActivity {
     private Button send;
     private RecyclerView msgRecyclerView;
     private MsgAdapter msgAdapter;
+    private long exitTime = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 //        getSupportActionBar().hide();
-//        getWindow().setFlags(WindowManager.LayoutParams.FILL_PARENT, WindowManager.LayoutParams.FILL_PARENT);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
         initMsg();
         inputText = (EditText) findViewById(R.id.et_message);
         send = (Button) findViewById(R.id.btn_send);
@@ -35,13 +39,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String content = inputText.getText().toString();
-                if (!"".equals(content)){
+                /*if (!"".equals(content)){
                     Msg msg = new Msg(content, Msg.TYPE_SENT);
                     msgList.add(msg);
                     msgAdapter.notifyItemInserted(msgList.size() - 1);
                     msgRecyclerView.scrollToPosition(msgList.size() - 1);
                     inputText.setText("");
-                }
+                }*/
+                Utils.SendToServer(1,content,"send");
             }
         });
     }
@@ -52,5 +57,20 @@ public class MainActivity extends AppCompatActivity {
         msgList.add(msg2);
         Msg msg3 = new Msg("fuck you", Msg.TYPE_RECEIVED);
         msgList.add(msg3);
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){
+            if ((System.currentTimeMillis() - exitTime) > 2000){
+                Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT)
+                        .show();
+                exitTime = System.currentTimeMillis();
+            }else {
+                finish();
+                System.exit(0);
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
---
+++ app/src/main/java/com/project/hzwlcd/english_solitaire/Utils.java
@@ -0,0 +1,56 @@
+package com.project.hzwlcd.english_solitaire;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Project EnglishSolitaire/com.project.hzwlcd.english_solitaire/Utils
+ * Created by HZW
+ * Data 2017/10/19
+ * Time 13:55
+ */
+
+public class Utils {
+
+    private static String http_address = "http://192.168.23.102:8080";
+
+    public static String Get_Http_address(){
+        return http_address;
+    }
+
+    public static void setHttp_address(String http_address) {
+        http_address = http_address;
+    }
+
+    static void SendToServer(final int user_id, final String word, final String msg_type){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+//                    String path = Get_Http_address()+"/MyServer/Login";
+                    String path = "http://192.168.24.113:8080/MyServer/login";
+                    String data = "user_id="+user_id+"&word="+word+"&msg_type="+msg_type+"";
+
+                    URL url = new URL(path);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    System.out.println(connection);
+                    connection.setRequestMethod("POST");
+                    connection.setConnectTimeout(5000);
+                    connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                    connection.setRequestProperty("Content-Length", data.length()+"");
+                    connection.setDoOutput(true);
+                    connection.getOutputStream().write(data.getBytes());
+
+                    int code = connection.getResponseCode();
+
+                    if (code == 200){
+                        // TODO: 2017/10/19 你在这里写成功访问服务器时的操作
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                super.run();
+            }
+        }.start();
+    }
+}
